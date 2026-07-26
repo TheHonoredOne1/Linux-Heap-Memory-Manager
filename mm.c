f900f4738e0b89f15b35cf1b704977e74de4bf00
Sush_B <thehonoredone1@gmail.com>
2026-07-26 18:26:38 +0530

    improvisation in previous code

--- mm.c
+++ mm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <memory.h>
 #include <unistd.h>   /* for getpagesize() = 4096 no my system*/
 #include <sys/mman.h> /* for mmap() call*/
@@ -7,7 +8,7 @@
 static size_t SYSTEM_PAGE_SIZE = 0; // this is in bytes
 static vm_page_for_families_t *first_vm_page_for_families = NULL;
 
-void mm_init()
+void mm_initializer()
 {
     SYSTEM_PAGE_SIZE = getpagesize();
 }
@@ -42,18 +43,17 @@ void return_vm_page_to_kernel(void *vm_page, int units)
     else
     {
         printf("Error : Could not munmap VM page back to kernel.\n");
-        return NULL;
     }
 }
 
 void mm_instantiate_new_page_family(char *struct_name, __uint32_t struct_size)
 {
-    page_family_t *curr_family = NULL;
+    page_family_t *curr_family_iterator = NULL;
     vm_page_for_families_t *new_vm_page_for_families = NULL;
     if (struct_size > SYSTEM_PAGE_SIZE)
     {
         printf("Error : %s() Structure %s size exceeds the system page size\n",
-        __FUNCTION__, struct_name);
+               __FUNCTION__, struct_name);
         return;
     }
 
@@ -61,40 +61,40 @@ void mm_instantiate_new_page_family(char *struct_name, __uint32_t struct_size)
     {
         first_vm_page_for_families = (vm_page_for_families_t *)get_new_vm_page_from_kernel(1);
         first_vm_page_for_families->next = NULL;
-        strncpy(first_vm_page_for_families->page_family[0].struct_name, struct_name, MM_MAX_STRUCT_NAME);
+        strncpy(first_vm_page_for_families->page_family[0].struct_name, struct_name, MM_MAX_STRUCTNAME_SIZE);
         first_vm_page_for_families->page_family[0].struct_size = struct_size;
         return;
     }
 
-    __uint32_t count = 0;
-    ITERATE_PAGE_FAMILIES_BEGIN(first_vm_page_for_families, curr_family){
-        
-        if(strncmp(curr_family->struct_name, struct_name, MM_MAX_STRUCT_NAME) != 0)
+    __uint32_t count_of_families_in_vm_page = 0;
+    ITERATE_PAGE_FAMILIES_BEGIN(first_vm_page_for_families, curr_family_iterator)
+    {
+        if (strncmp(curr_family_iterator->struct_name, struct_name, MM_MAX_STRUCTNAME_SIZE) != 0)
         {
-            count++;
+            count_of_families_in_vm_page++;
             continue;
         }
         assert(0); // if the application registers the same page-family twice - error
+    }
+    ITERATE_PAGE_FAMILIES_END(first_vm_page_for_families, curr_family_iterator);
 
-    }ITERATE_PAGE_FAMILIES_END(first_vm_page_for_families, curr_family);
-
-    if(count == MAX_FAMILIES_PER_VM_PAGE)
+    if (count_of_families_in_vm_page == MAX_FAMILIES_PER_VM_PAGE)// we need a new page.
     {
-        vm_page_for_families_t * new_page =  (vm_page_for_families_t *)get_new_vm_page_from_kernel(1);
-        new_page->next =  first_vm_page_for_families;
+        vm_page_for_families_t *new_page = (vm_page_for_families_t *)get_new_vm_page_from_kernel(1);
+        new_page->next = first_vm_page_for_families;
         first_vm_page_for_families = new_page;
-        curr_family = &first_vm_page_for_families->page_family[0];
+        curr_family_iterator = &first_vm_page_for_families->page_family[0];
     }
 
-    curr_family->struct_size = struct_size;
-	strncpy(curr_family->struct_name,struct_name, MM_MAX_STRUCT_NAME);
+    curr_family_iterator->struct_size = struct_size;
+    strncpy(curr_family_iterator->struct_name, struct_name, MM_MAX_STRUCTNAME_SIZE);
     return;
 }
 
 int main()
 {
 
-    mm_init();
+    mm_initializer();
     printf("The size of the page on my system : %zu\n", SYSTEM_PAGE_SIZE);
 
     void *p1Add = get_new_vm_page_from_kernel(1);
--- mm.h
+++ mm.h
@@ -1,33 +1,36 @@
-#define MM_MAX_STRUCT_NAME 32
-
+#define MM_MAX_STRUCTNAME_SIZE 32
+typedef struct vm_page_for_families_ vm_page_for_families_t;
 typedef struct page_family_
 {
-    char struct_name[MM_MAX_STRUCT_NAME];
+    char struct_name[MM_MAX_STRUCTNAME_SIZE];
     __UINT32_TYPE__ struct_size;
 
 } page_family_t;
 
 typedef struct vm_page_for_families_
 {
-    struct vm_page_for_families_ *next;
+    vm_page_for_families_t *next;
     page_family_t page_family[0];
 
 } vm_page_for_families_t;
 
 #define MAX_FAMILIES_PER_VM_PAGE \
-    (SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) / sizeof(page_family_t)
-
+    ((SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) / sizeof(page_family_t))
 
      
-#define ITERATE_PAGE_FAMILIES_BEGIN(vm_page_for_families_ptr, curr)                 \
-{                                                                                   \
-    __uint32_t _count = 0;                                                            \
-    for (curr = (page_family_t *)&(vm_page_for_families_ptr->page_family[0]); \
-     _count < MAX_FAMILIES_PER_VM_PAGE && (curr->struct_size);                  \
-     _count++, curr++)                                                          \
+#define ITERATE_PAGE_FAMILIES_BEGIN(vm_page_for_families_ptr, curr_family_iterator)             \
+{                                                                               \
+    __uint32_t _count = 0;                                                      \
+    for (curr_family_iterator = (page_family_t *)&(vm_page_for_families_ptr->page_family[0]);   \
+     _count < MAX_FAMILIES_PER_VM_PAGE && (curr_family_iterator->struct_size);                  \
+     _count++, curr_family_iterator++)                                                          \
     {
+/*
+void* start_address = &(vm_page_for_families_ptr -> page_family[0]);
+    for (curr_family_iterator = (page_family_t *)start_address; 
+     _count < MAX_FAMILIES_PER_VM_PAGE && (curr_family_iterator->struct_size);           
+     _count++, curr_family_iterator++)                                                         
+    {
+*/
 
-
-#define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr) \
-    }                                                             \
-}
+#define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr_family_iterator) }}
