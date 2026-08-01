60421aafabf56b8b703c18c7c2268c5d50012536
Sush_B <thehonoredone1@gmail.com>
2026-08-01 17:49:16 +0530

    add meta block utility macros in mm.h

--- mm.h
+++ mm.h
@@ -37,4 +37,38 @@ void* start_address = &(vm_page_for_families_ptr -> page_family[0]);
 
 page_family_t* lookup_page_family_by_name(char * family_name);
 
+typedef enum {
+    MM_FALSE,
+    MM_TRUE
+} vm_bool_t;
 
+typedef struct block_meta_data_  block_meta_data_t;
+struct block_meta_data_{
+    vm_bool_t is_free;
+    __uint32_t data_block_size;
+    __uint32_t offset;
+    block_meta_data_t* previous_block;
+    block_meta_data_t* next_block;
+};
+
+typedef struct vm_page_ vm_page_t;
+struct vm_page_{
+    vm_page_t * next;
+    vm_page_t * prev;
+};
+
+#define OFFSET_OF(structure_type, field_name)\
+    ((size_t)&(((structure_type*)0)->field_name))
+
+#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)\
+    ((void*)((char*)block_meta_data_ptr - block_meta_data_ptr->offset))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr)\
+    (block_meta_data_ptr->next_block)
+
+#define PREV_META_BLOCK(block_meta_data_ptr)\
+    (block_meta_data_ptr->previous_block)
+
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)\
+    ((void*)((char*)block_meta_data_ptr + (sizeof(block_meta_data_t) + (block_meta_data_ptr->data_block_size))))
+    
